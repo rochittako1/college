37f8ca2de3df0b17892ea0f01bd62bd0b67ba452
Rochit Tako <rochittako1@gamil.com>
2025-11-21 13:16:31 +0545

    Added math operation

--- MathOperations.java
+++ MathOperations.java
@@ -10,10 +10,20 @@ public class MathOperations
 {
     public static void main(String[] args){
     Scanner scan= new Scanner(System.in);
-
-    
-    
-    
+    System.out.println("enter first number:");
+    int a=scan.nextInt();
+    System.out.println("enter Second number:");
+    int b=scan.nextInt();
+    int sum=a+b;
+    int diff=a-b;
+    int mul=a*b;
+    int div=a/b;
+    System.out.println("sum:");
+    System.out.println("Diiference:");
+    System.out.println("Multipication:");
+    System.out.println("Dvision");
+    System.out.println("post-increment",a++);
+    System.out.println("Pre-increment:",++a);
     
     
     }
